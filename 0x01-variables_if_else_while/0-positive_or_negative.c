ea5213b6fb275c9e2f4854322e37af28e6c97bd6
Tonnyderrick <tonnyaketch@gmail.com>
2025-02-08 02:23:48 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,13 @@
  *
  */
 
-int main(void)
-
 {
-
   int n;
 
   srand(time(0));
-  n = rand() - RAND_MAX / 2
+  n = rand() - RAND_MAX / 2;
 
-
-  if (n > 0>
+  if (n > 0)
     {
       printf("%d is positive\n", n);
     }
@@ -34,5 +30,3 @@ int main(void)
       printf("%d is negative\n", n);
     }
   return (0);
-
-}
