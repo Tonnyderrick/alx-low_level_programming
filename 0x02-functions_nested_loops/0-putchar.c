5c8bc370a8dc559655f48d35139d2fd9bfce52c0
Tonnyderrick <tonnyaketch@gmail.com>
2025-02-18 09:43:30 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-/**
-*main - prints _putchar followed by new line
-*Return: Always 0 (Success)
+/**                                                                                                                                                                                                                        
+*main - prints _putchar followed by new line                                                                                                                                                                               
+*Return: Always 0 (Success)                                                                                                                                                                                                
 */
 
 int main(void)
 {
 	char str[] = "_putchar";
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	_putchar(str[i]);
