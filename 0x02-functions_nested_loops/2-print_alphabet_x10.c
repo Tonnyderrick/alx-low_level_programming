11b7aa97ad61bdfc04b3aafce71621f8ec384a21
Tonnyderrick <tonnyaketch@gmail.com>
2025-02-18 10:23:40 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times, in lowercase
+ * followed by a new line.
+ */
+
+void print_alphabet_x10(void)
+{
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; alphabet[j] != '\0'; j++)
+        {
+            _putchar(alphabet[j]);
+        }
+        _putchar('\n');
+    }
+}
