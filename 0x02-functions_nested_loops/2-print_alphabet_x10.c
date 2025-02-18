2be71d0775e719f5af46e13b9bee00d432112830
Tonnyderrick <tonnyaketch@gmail.com>
2025-02-18 10:44:36 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* prints_alphabet_x10 - prints the alphabet 10 times, in lowercase
+* print_alphabet_x10 - prints the alphabet 10 times, in lowercase
 * followed by a new line.
 */
 void print_alphabet_x10(void)
