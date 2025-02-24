e85b76f06bee9c5023deb2c18a58efeb541f2fe3
Tonnyderrick <tonnyaketch@gmail.com>
2025-02-24 00:47:41 -0800

    checks for upper alphabets

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - Checks if a character is uppercase
- * @c: input character
- *
- * Return: (1) for true and (0) for false
- */
+* _supper - Checks if a character is uppercase
+* @c: input character
+*
+* Return:(1) for true and (0) for false
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	  {
+	{
 	return (1);
-	  }
+	}
 	else
-	  {
+	{
 	return (0);
-	  }
+	}
 }
