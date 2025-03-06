d8846f08da2f45eb3f625e4871b4abbcde853370
Tonnyderrick <tonnyaketch@gmail.com>
2025-03-06 02:45:07 -0800

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,6 @@
  * _strcat - Concatenates two strings.
  * @dest: The destination string.
  * @src: The source string.
- *
  * Return: A pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
