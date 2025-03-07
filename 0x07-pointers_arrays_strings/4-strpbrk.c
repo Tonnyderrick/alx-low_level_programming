5d88322019295fb7618078a2357be8c6f7e3202e
Tonnyderrick <tonnyaketch@gmail.com>
2025-03-07 04:52:13 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * _strpbrk - Searches a string for any of a set of bytes
  * @s: The string to be scanned
  * @accept: The string containing the characters to match
- * 
+ *
  * Return: Pointer to the byte in s that matches one of the bytes in accept,
  *         or NULL if no such byte is found
  */
