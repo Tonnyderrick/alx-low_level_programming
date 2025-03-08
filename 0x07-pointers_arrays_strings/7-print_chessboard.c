d04308620efe14b038195e32680d148d1105c7b7
Tonnyderrick <tonnyaketch@gmail.com>
2025-03-08 00:48:14 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,13 +5,13 @@
  * @a: 2D array representing the chessboard
  * Return: Always 0 (success)
  */
-void print_chessboard(char (*a)[8]) 
+void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
-	for (j = 0; j < 8; j++) 
+	for (j = 0; j < 8; j++)
 	{
 	putchar(a[i][j]);
 	}
